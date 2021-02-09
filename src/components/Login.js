b27f15038d32360afa3ff2cf81b6cc99d2820685
Benson <48788513+benson00077@users.noreply.github.com>
2021-02-10 01:17:52 +0800

    Add Login CSS

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Route, Link, Redirect, useLocation, useHistory } from 'react-router-dom';
 import axios from 'axios'
+import styles from './login.module.css'
 
 function PrivateRoute({ children, ...rest }) {
     return (
@@ -49,20 +50,12 @@ function Login( {setIsLoggedIn, setUser} ) {
     
     
     return (
-      <div className="login-wrapper">
-        <h1>Please Log In</h1>
+      <div className={styles.login}>
+        <h1>Log In</h1>
         <form>
-          <label>
-            <p>Username</p>
-            <input onChange={e => setUserName(e.target.value)}/>
-          </label>
-          <label>
-            <p>Password</p>
-            <input onChange={e => setPassword(e.target.value)}/>
-          </label>
-          <div>
-            <button onClick={login}>Submit</button>
-          </div>
+            <input type="text" onChange={e => setUserName(e.target.value)}/>
+            <input type="password" onChange={e => setPassword(e.target.value)}/>
+            <button type="submit" onClick={login}> Let me in </button>
         </form>
         <h2>{loginDescription}</h2>
       </div>
