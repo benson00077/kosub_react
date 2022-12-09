220381c328230817c94beb55b8c31d7201079fe2
Benson <48788513+benson00077@users.noreply.github.com>
2022-12-09 21:32:43 +0800

    refactor: search bar

---
+++ src/components/SearchBar/Circles.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Circle.module.css'
+
+function Circles() {
+  return (
+    <div className={styles.threeCircles}></div>
+  )
+}
+
+export default Circles
--- src/components/SearchBar/LanguageSelector.js
+++ src/components/SearchBar/LanguageSelector.js
@@ -15,15 +15,19 @@ function LanguageSelector({ value }) {
   }
 
   return (
-    <React.Fragment>
-      <h3 className="isActive"> Search In </h3>
+    <div className="flex flex-row justify-end w-8 flex-grow font-color">
+      {/* <h3 className="isActive"> Search In </h3> */}
+      <h3 className="px-5 py-5 text-amber-50 border-b-2 border-stone-400"> Search In </h3>
       {value.map((each, i) => (
-        <h3 className={isActive[i] ? 'isActive' : ''} key={i} onClick={() => clickHandler(each, i)}>
+        // <h3 className={isActive[i] ? 'isActive px-5 py-5 ' : 'px-5 py-5'} 
+        <h3 className={`${isActive[i] ? 'text-zinc-50 border-b-2 border-stone-400' : 'text-zinc-400'} px-5 py-5`} 
+          key={i} 
+          onClick={() => clickHandler(each, i)}>
           {' '}
           {checkLabel[i]}{' '}
         </h3>
       ))}
-    </React.Fragment>
+    </div>
   )
 }
 
