536e3ef95604f8f35c548dc4bd56abb89ce5133a
Benson <48788513+benson00077@users.noreply.github.com>
2022-12-10 00:08:44 +0800

    fix: layout of card
    Set footer button aligned to bottom right
    Set gap between header and main section

--- src/components/Pages/Shared/Card.js
+++ src/components/Pages/Shared/Card.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Card({ children }) {
   return (
-    <div className="p-4 bg-sky-600/10 hover:bg-gray-600/80 hover:scale-105 hover:transition-all rounded-lg border border-stone-700 h-[30vh] overflow-y-scroll no-scrollbar">
-      <header className="text-lg">{children[0]}</header>
+    <div className="p-4 bg-sky-600/10 hover:bg-gray-600/80 hover:scale-105 hover:transition-all rounded-lg border border-stone-700 h-[30vh] overflow-y-scroll no-scrollbar relative">
+      <header className="text-lg h-14 mb-12">{children[0]}</header>
       <main className="h-28">{children[1]}</main>
-      <footer>{children[2]}</footer>
+      <footer className="absolute bottom-4 right-4">{children[2]}</footer>
     </div>
   )
 }
