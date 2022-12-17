c91163539d87f44204cafd77b1821b4eca10cbab
Benson <48788513+benson00077@users.noreply.github.com>
2022-12-17 23:54:06 +0800

    fix: side menu different init state for pc and mobile

--- src/App.js
+++ src/App.js
@@ -23,7 +23,11 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [user, setUser] = useState('')
 
-  const [showSideMenu, setShowSideMenu] = useState(false)
+  const [showSideMenu, setShowSideMenu] = useState(() => {
+    const isMobile = window.innerWidth <= 768 // iPad Mini
+    if (isMobile) return false
+    if (!isMobile) return true
+  })
 
   useEffect(() => {
     const grettingsStyle =
@@ -107,10 +111,18 @@ function App() {
               </div>
             </div>
             <div
-              className="absolute left-[5vw] bottom-[5vh] z-20 hover:cursor-pointer"
+              className="absolute left-[7vw] md:left-[6vw] bottom-[5vh] z-20 hover:cursor-pointer"
               onClick={(e) => setShowSideMenu(!showSideMenu)}
             >
-              ⚙
+              <svg
+                class={`w-4 md:w-7 ${
+                  showSideMenu ? 'fill-neutral-50/80' : 'fill-slate-300/80'
+                } transition-all hover:scale-110 hover:opacity-50`}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M7 3H2v14h5V3zm2 0v14h9V3H9zM0 3c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3zm3 1h3v2H3V4zm0 3h3v2H3V7zm0 3h3v2H3v-2z" />
+              </svg>
             </div>
           </div>
         </UseContextProvider>
