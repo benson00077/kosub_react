7157c123a59faccf77f3476fadf2303522b9e521
Benson <48788513+benson00077@users.noreply.github.com>
2023-07-28 17:24:55 +0800

    feat: style for active nav

---
+++ src/data/navigations.js
@@ -0,0 +1,26 @@
+export const NAV = {
+  HOME: {
+    path: '/',
+    title: 'Home',
+  },
+  ABOUT: {
+    path: '/about',
+    title: 'About',
+  },
+  SEARCH: {
+    path: '/search',
+    title: 'Search',
+  },
+  LOGIN: {
+    path: '/login',
+    title: 'Log in',
+  },
+  REGISTER: {
+    path: '/register',
+    title: 'Create',
+  },
+  FAVORITE: {
+    path: '/mysentences',
+    title: 'Favorite Sentences',
+  },
+}
